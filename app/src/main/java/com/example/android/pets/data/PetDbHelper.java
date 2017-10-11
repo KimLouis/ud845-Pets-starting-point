95a0c76158e06c5c68680318d434b7f74dbcdcde
KimLouis <qinwen_luo@outlook.com>
2017-10-11 15:26:57 +0800

    1. Creating Class petDbHelper extends from SQLiteOpenHelper 2. Adding displayDatabaseInfo() in CatalogActivity

---
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,54 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import static com.example.android.pets.data.PetContract.PetEntry;
+
+/**
+ * Created by Qinwen on 2017/10/11.
+ */
+
+public class PetDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = PetDbHelper.class.getSimpleName();
+
+    /**
+     * Name of database file.
+     */
+    private static final String DATABASE_NAME = "shelter.db";
+
+    /**
+     * Database version, increment when schema upgrades.
+     */
+    private static final int DATABASE_VERSION = 1;
+
+    public PetDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_ENTRIES = "CREATE TABLE " + PetEntry.TABLE_NAME
+                + "(" + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
+                + PetEntry.COLUMN_PET_BREED + " TEXT, "
+                + PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, "
+                + PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+
+        db.execSQL(SQL_CREATE_ENTRIES);
+
+        Log.v(LOG_TAG, SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+//        db.execSQL(SQL_DELETE_ENTRIES);
+//        onCreate(db);
+    }
+
+}
